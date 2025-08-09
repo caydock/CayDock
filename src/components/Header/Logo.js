68e23614cbbda56bf3a368ead09dadf1e9207d64
Cay <w3cay@hotmail.com>
2025-08-09 10:02:33 +0800

    ui(header): center the site logo in header
    - Center header container with justify-center
    - Remove left/right margins on logo wrapper and adjust size

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,14 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
-import profileImg from "@/public/profile-img.png"
+import profileImg from "@/public/logo.webp"
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
+    <Link href="/" className="flex items-center justify-center text-dark dark:text-light">
+        <div className=" w-40 md:w-32 rounded-full overflow-hidden dark:border-gray">
+            <Image src={profileImg} alt="w3cay logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
-        <span className="font-bold dark:font-semibold text-lg md:text-xl">CodeBucks</span>
     </Link>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="w-full p-6 px-5 sm:px-10 flex items-center justify-start">
+    <header className="w-full p-6 px-5 sm:px-10 flex items-center justify-center">
       <Logo />
     </header>
   )
