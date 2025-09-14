3f0d2031ef79c7fa9a636db1e2068c25f7d54f2f
Cay <w3cay@hotmail.com>
2025-09-14 20:56:55 +0800

    fix: update Umami script source and website ID in Analytics component

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -25,7 +25,7 @@ gtag('config', 'G-GY58MNR1C0');
       y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
   })(window, document, "clarity", "script", "soy2grvr91");
  `}</Script>
- <Script defer src="https://cloud.umami.is/script.js" data-website-id="c7ffa851-1275-406a-b593-78a51e851f87"></Script>
+ <Script defer src="https://umami.caydock.com/script.js" data-website-id="0e516cec-cb7c-42dc-bc80-679b4f00adeb"></Script>
     </>
   )
 }
